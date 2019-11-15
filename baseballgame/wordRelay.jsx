7983f320f60fd6f8415bcbc1c71cc3cdebe73fb6
jeonghunSakong <sgjh4040@naver.com>
2019-11-15 18:11:27 +0900

    baseball

---
+++ baseballgame/wordRelay.jsx
@@ -0,0 +1,19 @@
+
+const React = require('react');
+const {Component} = React;
+
+
+class WordRelay extends Component{
+    state ={
+        text:"hello"
+
+    };
+    render() {
+        return(
+            <div>{this.state.text}</div>
+        )
+
+    }
+}
+
+module.exports = WordRelay;
